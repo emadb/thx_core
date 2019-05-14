e6006ca261fea5d2188c796964c192c311e329fa
Emanuele DelBono <emanuele@codiceplastico.com>
2019-05-14 21:29:43 +0200

    more tests

--- test/schedule_process_test.exs
+++ test/schedule_process_test.exs
@@ -12,9 +12,16 @@ defmodule ThxCore.ScheduleProcessTest do
     Test.ThermostatWriterMock
     |> expect(:switch_on, fn _ -> :ok end)
 
-    ## TODO: 1 is not always valid, we need to query the database to know the id??
     ThxCore.ScheduleProcess.start_link(1, "one")
-
     ThxCore.ScheduleProcess.update_thermostat("one", 5)
+  end
+
+  test "Temperature is over threshold, thermostat should do nothing" do
+
+    Test.ThermostatWriterMock
+    |> expect(:switch_on, 0, fn _ -> :ok end)
+
+    ThxCore.ScheduleProcess.start_link(1, "one")
+    ThxCore.ScheduleProcess.update_thermostat("one", 25)
   end
 end
